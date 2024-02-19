9f8d30a81abb92addba1f5a8b3d9a2d18f7cbb9a
hisomura <hisomura.noreply@gmail.com>
2024-02-20 06:37:20 +0900

    43 - Exclude

---
+++ answers/00043-easy-exclude.ts
@@ -0,0 +1,42 @@
+/**
+ * @see https://github.com/type-challenges/type-challenges/blob/main/questions/00043-easy-exclude/README.md
+ */
+/*
+  43 - Exclude
+  -------
+  by Zheeeng (@zheeeng) #easy #built-in #union
+
+  ### Question
+
+  Implement the built-in `Exclude<T, U>`
+
+  > Exclude from `T` those types that are assignable to `U`
+
+  For example:
+
+  ```ts
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+  ```
+
+  > View on GitHub: https://tsch.js.org/43
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyExclude<T, U> = T extends (infer Result) | U ? Result extends U ? never : Result : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
+  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/43/answer
+  > View solutions: https://tsch.js.org/43/solutions
+  > More Challenges: https://tsch.js.org
+*/
